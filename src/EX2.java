4c251b996d84949ed85b993540e2f72fe8a647c0
Jul1nh0 <108277816+Jul1nh0@users.noreply.github.com>
2022-08-26 12:17:28 +0300

    1

---
+++ src/EX2.java
@@ -0,0 +1,16 @@
+public class EX2 {
+    public static void main(String[] args) {
+        int balance = 40;
+        int money = 1356;
+        int bonus;
+
+       if(money > 1000) {
+           bonus = money / 100;
+       } else {
+           bonus = 0;
+       }
+       int totalBalance = balance + money + bonus;
+        System.out.println("Ваш баланс равен: " + totalBalance);
+        System.out.println("Бонусов начислено: " + bonus);
+    }
+}
